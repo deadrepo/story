1659d97c5131327ceb62eecd9608b3f676e26147
h4x00r <50273964+4zer0day@users.noreply.github.com>
2019-05-18 19:10:48 +0800

    Add files via upload

---
+++ story.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+clear
+
+echo "" 
+echo ""  
+COLUMNS=$(tput cols) 
+title="LIFE FROM MY POINT OF VIEW"  
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+	         
+echo -e ""
+echo -e ""  
+	                                                                                                
+COLUMNS=$(tput cols) 
+title="Press Space Bar to Read..."
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+read -n 1
+clear
+echo "I want to lean on someone, but I don't have anyone (used to, not anymore)"
+read -n 1
+clear
+echo "I want to lay down on the cloud in the sky (unreachable) "
+read -n 1
+clear
+echo "Maybe it's just time to go, I let it go myself"
+read -n 1
+clear
+echo "If this is a sin, I have nowhere else to go"
+read -n 1
+clear
+echo "Nowhere else to go..."
+read -n 1
+clear
+echo "More than your short bliss, I was more important to myself"
+read -n 1
+clear
+echo "Rather than the truth you needed to know, I showed a lie"
+read -n 1
+clear
+echo "Like this world that only shows me good things"
+read -n 1
+clear
+echo "With a small palm, I want to cover your eyes"
+read -n 1
+clear
+echo "You might think it's unfair, but we're all victims"
+read -n 1
+clear
+echo "Once you're born and fall in love, You need to experience pain"
+read -n 1
+clear
+echo "That's the reality"
+read -n 1
+clear
+echo "We'll dissapear like a black dot"
+read -n 1
+clear
+echo "When the passionate words cools down (love)"
+read -n 1
+clear
+echo "Even if I donn't see you"
+read -n 1
+clear
+echo "I know why you're so down (tired of me or found someone new?)"
+read -n 1
+clear
+echo "I know, but I ignore it"
+read -n 1
+clear
+echo "Because that's easier, like a spoiler"
+read -n 1
+clear
+echo "I didn't wanna know actually"
+read -n 1
+clear
+echo "But the one who showed that look was"
+read -n 1
+clear
+echo "You, who?, who?, who?"
+read -n 1
+clear
+echo "Yeah you, N"
+read -n 1
+clear
+echo "After I lock away the tears"
+read -n 1
+clear
+echo "Shits happen, you did it again"
+read -n 1
+clear
+echo "1/6/2018/5/2019"
+read -n 1
+clear
+echo "" 
+echo "" 
+COLUMNS=$(tput cols) 
+title="THE END"
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+
+
+
+
+
+
+
